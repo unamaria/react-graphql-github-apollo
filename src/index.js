67f56668fbecc09ea33d501fd6ff8a734f83ab02
Maria Comas <unamaria@gmail.com>
2018-12-09 18:21:02 +0100

    Add and configure Apollo Client.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink} from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 import './style.css';
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+// eslint-disable-next-line no-unused-vars
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
 
 ReactDOM.render(
   <App />,
